2651348590f869845fab10fd4782a752c7383e1d
GiovanneAndrade <vanne.as@hotmail.com>
2022-08-29 17:27:20 -0300

    ajuste final

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ server.use(express.json());
 let sign = []
 let tweets = []
 const salvartweet = []
-let tweetnovo = []  
-
-// -------metodo GET--------------------------------
+ 
 server.get('/tweets', function(req, res) {
   if(tweets.length<=10){
     res.send(tweets.reverse());
@@ -18,49 +16,24 @@ server.get('/tweets', function(req, res) {
     const tweetsReturn = tweets.slice(tweets.length-10, tweets.length);
     res.send(tweetsReturn.reverse());
   }
-  
-
- 
-
 });
-//---------------------------------------------------
 
-
-
-//------------METOD POST USUARIO------------------------
 server.post('/sign-up', function(req, res) {
-//req.body.id = sign.length + 1;
-
-
 sign.push(req.body)
 res.send('ok');
 })
-//-------------------------------------------------
-
-
-
-//----------POST TWEET----------------------------------
 server.post('/tweets', function(req, res) {
-  //req.body.id = tweets.length + 1;
  const tweet = req.body.tweet
-  let [username, avatar] = sign
-  
-    let tweetusername = {...{tweet}, ...username,  ...avatar}
-  
- 
+ let [username, avatar] = sign
+ let tweetusername = {...{tweet}, ...username,  ...avatar}
   if(!username){
     return res.send('faça login')
   }else{
     tweets.push(tweetusername)
   }
-
   salvartweet.push(req.body)
-
 res.send('ok');
 });
-//-------------------------------------------------
-
-
 
 server.listen(5000, function() {
   console.log('tudo ok')
