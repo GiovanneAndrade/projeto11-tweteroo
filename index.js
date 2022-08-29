6b6590252c689f960e2b31155e3cf35406045599
GiovanneAndrade <vanne.as@hotmail.com>
2022-08-29 20:46:04 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 
-
 const server = express();
 server.use(cors());
 server.use(express.json());
@@ -10,22 +9,19 @@ let tweets = []
 const salvartweet = []
  
 server.get('/tweets', function(req, res) {
-  if(tweets.length<=10){
-    res.send(tweets.reverse());
-  }else {
-    const tweetsReturn = tweets.slice(tweets.length-10, tweets.length);
-    res.send(tweetsReturn.reverse());
-  }
+   const ultimosdez = tweets.slice(-10)
+    res.send(ultimosdez.reverse());
 });
 
 server.post('/sign-up', function(req, res) {
-sign.push(req.body)
-res.send('ok');
+  sign.push(req.body)
+  res.send('ok');
 })
+
 server.post('/tweets', function(req, res) {
  const tweet = req.body.tweet
  let [username, avatar] = sign
- let tweetusername = {...{tweet}, ...username,  ...avatar}
+ let tweetusername = {...{tweet}, ...username, ...avatar}
   if(!username){
     return res.send('faça login')
   }else{
